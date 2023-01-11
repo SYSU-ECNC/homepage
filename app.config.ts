68bbab4234e56efa252a03ff32ccff07d5183ab2
Robin Wong <chaowang0313@gmail.com>
2023-01-11 05:48:22 +0000

    feat(docs): init docus

---
+++ app.config.ts
@@ -0,0 +1,22 @@
+export default defineAppConfig({
+  docus: {
+    title: 'ECNC',
+    description: '网管技术分享',
+    // image: '',
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      credits: {
+        text: '粤ICP备19084931号',
+        href: 'https://beian.miit.gov.cn',
+      },
+    }
+  }
+})
